3f2f425ba71b11cbe8bc3f0b62aad8bd12691de8
Patrycja Holda <patrycjafirma@gmail.com>
2024-12-30 01:59:59 +0100

    changed "let" to "const" in JS & added new const "calculateResult" based on > switch, case

--- JS/script.js
+++ JS/script.js
@@ -1,47 +1,44 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let buttonElement = document.querySelector(".js-button");
-let resultElement = document.querySelector(".js-result");
+{
+  const formElement = document.querySelector(".js-form");
+  const amountElement = document.querySelector(".js-amount");
+  const currencyElement = document.querySelector(".js-currency");
+  const buttonElement = document.querySelector(".js-button");
+  const resultElement = document.querySelector(".js-result");
 
-buttonElement.addEventListener("click", () => {
-  buttonElement.innerHTML = "OBLICZ PONOWNIE";
-});
+  const calculateResult = (amount, currency) => {
+    const rateEUR = 4.29;
+    const rateGBP = 5.02;
+    const rateUSD = 4.03;
+    const rateCHF = 4.41;
+    const rateTHB = 0.11;
 
-let rateEUR = 4.29;
-let rateGBP = 5.02;
-let rateUSD = 4.03;
-let rateCHF = 4.41;
-let rateTHB = 0.11;
-
-formElement.addEventListener("submit", (event) =>
-  {
-    event.preventDefault();
-  
-  let amount = +amountElement.value;
-  let currency = currencyElement.value;
-  
-  let result;
-  
-  switch (currency) {
-    case "EUR":
-      result = amount / rateEUR;
-      break;
-    case "USD":
-      result = amount / rateUSD;
-      break;
-    case "GBP":
-      result = amount / rateGBP;
-      break;
-    case "CHF":
-      result = amount / rateCHF;
-      break
-    case "THB":
-      result = amount / rateTHB;
-      break;
+    switch (currency) {
+      case "EUR":
+        return amount / rateEUR;
+      case "USD":
+        return amount / rateUSD;
+      case "GBP":
+        return amount / rateGBP;
+      case "CHF":
+        return amount / rateCHF;
+      case "THB":
+        return amount / rateTHB;
+    }
   }
-  
-  resultElement.innerHTML = `<section class="section">Wynik: ${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}
+
+  buttonElement.addEventListener("click", () => {
+    buttonElement.innerHTML = "OBLICZ PONOWNIE";
+  });
+
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
+
+    const result = calculateResult(amount, currency);
+
+    resultElement.innerHTML = `<section class="section">Wynik: ${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}
   <div class="section__paragraphContainer">
   <p>KURSY WALUT NA DZIEŃ 12.04.24 r. </p>
   <p>EURO = 4.29</p>
@@ -49,8 +46,10 @@ formElement.addEventListener("submit", (event) =>
   <p>FUNT BRYTYJSKI = 5.02</p>
   <p>FRANK SZWAJCARSKI = 4.41</p>
   <p>BAT TAJLANDZKI = 0.11</p></div></section>`;
-});
+  });
 
-formElement.addEventListener("reset", () => {
-  resultElement.innerHTML = ""; 
-});
+  formElement.addEventListener("reset", () => {
+    resultElement.innerHTML = "";
+  });
+
+}
