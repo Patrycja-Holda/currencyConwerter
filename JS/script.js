f9645ca9594420d958d223aa770572d0954e2300
Patrycja Holda <patrycjafirma@gmail.com>
2024-12-30 02:50:59 +0100

    split code JS to function with "cost"

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,4 @@
 {
-  const formElement = document.querySelector(".js-form");
-  const amountElement = document.querySelector(".js-amount");
-  const currencyElement = document.querySelector(".js-currency");
-  const buttonElement = document.querySelector(".js-button");
-  const resultElement = document.querySelector(".js-result");
-
   const calculateResult = (amount, currency) => {
     const rateEUR = 4.29;
     const rateGBP = 5.02;
@@ -26,30 +20,50 @@
     }
   }
 
+  const updateResultText = (amount, result, currency) => {
+    const resultElement = document.querySelector(".js-result");
+    resultElement.innerHTML = `<section class="section">Wynik: ${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}
+ <div class="section__paragraphContainer">
+ <p>KURSY WALUT NA DZIEŃ 12.04.24 r. </p>
+ <p>EURO = 4.29</p>
+ <p>DOLAR = 4.03</p>
+ <p>FUNT BRYTYJSKI = 5.02</p>
+ <p>FRANK SZWAJCARSKI = 4.41</p>
+ <p>BAT TAJLANDZKI = 0.11</p></div></section>`;
+
+ const buttonElement = document.querySelector(".js-button");
+
   buttonElement.addEventListener("click", () => {
     buttonElement.innerHTML = "OBLICZ PONOWNIE";
   });
 
-  formElement.addEventListener("submit", (event) => {
+  }
+
+  const onFormSubmit = (event) => {
     event.preventDefault();
+
+    const amountElement = document.querySelector(".js-amount");
+    const currencyElement = document.querySelector(".js-currency");
 
     const amount = +amountElement.value;
     const currency = currencyElement.value;
 
     const result = calculateResult(amount, currency);
 
-    resultElement.innerHTML = `<section class="section">Wynik: ${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}
-  <div class="section__paragraphContainer">
-  <p>KURSY WALUT NA DZIEŃ 12.04.24 r. </p>
-  <p>EURO = 4.29</p>
-  <p>DOLAR = 4.03</p>
-  <p>FUNT BRYTYJSKI = 5.02</p>
-  <p>FRANK SZWAJCARSKI = 4.41</p>
-  <p>BAT TAJLANDZKI = 0.11</p></div></section>`;
-  });
+    updateResultText(amount, result, currency);
 
-  formElement.addEventListener("reset", () => {
-    resultElement.innerHTML = "";
-  });
+  }
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", onFormSubmit);
+
+    formElement.addEventListener("reset", () => {
+      resultElement.innerHTML = "";
+    });
+
+  }
+
+  init();
 
 }
